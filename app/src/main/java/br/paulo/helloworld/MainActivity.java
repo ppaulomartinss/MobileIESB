cfb86b2a723c856bb33957db40fd3097b6fc8739
Paulo Sérgio <sergiomartinsbsb@gmail.com>
2017-08-10 16:36:11 -0300

    Validações

--- app/src/main/java/br/paulo/helloworld/MainActivity.java
+++ app/src/main/java/br/paulo/helloworld/MainActivity.java
@@ -4,14 +4,61 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editNome;
+    EditText editEmail;
+    EditText editSenha;
+    EditText editConfirmarSenha;
+    Button btnConfirmar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editNome = (EditText) findViewById(R.id.editNome);
+        editEmail = (EditText) findViewById(R.id.editEmail);
+        editSenha = (EditText) findViewById(R.id.editSenha);
+        editConfirmarSenha = (EditText) findViewById(R.id.editConfirmarSenha);
+        btnConfirmar = (Button) findViewById(R.id.btnConfirmar);
+
+        btnConfirmar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                /*if(editNome.getText().length()==0 || editEmail.getText().length()==0) {
+                    Toast.makeText(getApplication(), "Preencha todos os campos", Toast.LENGTH_LONG).show();*/
+
+                if(editNome.getText().toString().length()==0){
+                    editNome.setError("Digite o seu nome!");
+
+                }
+                if(editEmail.getText().toString().length()==0) {
+                    editEmail.setError("Digite o seu E-mail!");
+                }
+
+                if(editSenha.getText().toString().length()==0) {
+                    editSenha.setError("Digite sua senha!");
+                }
+
+                if(editConfirmarSenha.getText().toString().length()==0){
+                    editConfirmarSenha.setError("Confirme sua senha!");
+
+                }else{
+                    Toast.makeText(getApplication(),
+                            "Cadastro realizado com sucesso " +editNome.getText().toString()+"!", Toast.LENGTH_LONG).show();
+
+                    //Limpando os dados
+
+                }
+            }
+
+        });
     }
 
     public void mostrarMensagem(View parametro){
@@ -22,4 +69,5 @@ public class MainActivity extends AppCompatActivity {
         Intent it = new Intent(this, LoginActivity.class);
         startActivity(it);
     }
+
 }
